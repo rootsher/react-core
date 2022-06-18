f809e56bf5c26a39072ef28aec940bc7d5fce2cd
Konrad Kowalski <rootsher@gmail.com>
2022-06-18 18:45:42 +0200

    feat: create example test file

---
+++ lib/index.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+
+import { add, Component } from './index';
+
+test("renders button", async () => {
+    render(<Component />);
+
+    const $button = await screen.findByTestId("button");
+
+    expect($button).toHaveTextContent('button');
+});
+
+test("adds numbers", () => {
+    expect(add(1, 2)).toBe(3);
+});
---
+++ lib/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Component() {
+    return <button data-testid="button">button</button>;
+}
+
+export function add(a: number, b: number) {
+    return a + b;
+}
